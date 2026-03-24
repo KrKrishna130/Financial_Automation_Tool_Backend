326d04a92f1c1b4dd91496d873af7b21f61cd2bd
krkrishna130 <krishnasoft1808@gmail.com>
2026-03-24 11:52:48 +0530

    manual getter setter made in entity

--- src/main/java/com/financeautomationmini/entity/TransactionData.java
+++ src/main/java/com/financeautomationmini/entity/TransactionData.java
@@ -15,10 +15,10 @@ import lombok.Setter;
 import java.time.LocalDate;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
 public class TransactionData {
 
         @Id
@@ -35,4 +35,78 @@ public class TransactionData {
         private boolean highValue;
         private String month;
 
+        public Double getGstAmount() {
+                return gstAmount;
+        }
+
+        public void setGstAmount(Double gstAmount) {
+                this.gstAmount = gstAmount;
+        }
+
+        public Long getId() {
+                return id;
+        }
+
+        public void setId(Long id) {
+                this.id = id;
+        }
+
+        public LocalDate getDate() {
+                return date;
+        }
+
+        public void setDate(LocalDate date) {
+                this.date = date;
+        }
+
+        public String getPartyName() {
+                return partyName;
+        }
+
+        public void setPartyName(String partyName) {
+                this.partyName = partyName;
+        }
+
+        public Double getAmount() {
+                return amount;
+        }
+
+        public void setAmount(Double amount) {
+                this.amount = amount;
+        }
+
+        public Double getGstPercent() {
+                return gstPercent;
+        }
+
+        public void setGstPercent(Double gstPercent) {
+                this.gstPercent = gstPercent;
+        }
+
+        public String getInvoiceType() {
+                return invoiceType;
+        }
+
+        public void setInvoiceType(String invoiceType) {
+                this.invoiceType = invoiceType;
+        }
+
+        public boolean isHighValue() {
+                return highValue;
+        }
+
+        public void setHighValue(boolean highValue) {
+                this.highValue = highValue;
+        }
+
+        public String getMonth() {
+                return month;
+        }
+
+        public void setMonth(String month) {
+                this.month = month;
+        }
+
+        public TransactionData() {
+        }
 }
